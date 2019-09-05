d72b4276581a238307aed843097a12694a9f296f
Zijie Liu <pliudev@outlook.com>
2019-09-05 07:54:44 -0500

    Update bash_init.sh

--- Setup/bash_init.sh
+++ Setup/bash_init.sh
@@ -55,12 +55,9 @@ gem install bundler
 bundle install
 
 cd Downloads
-# clone
 git clone https://github.com/powerline/fonts.git --depth=1
-# install
 cd fonts
 ./install.sh
-# clean-up a bit
 cd ..
 rm -rf fonts
 
